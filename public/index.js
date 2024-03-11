af571fd6d174d7fc248b6bd30464835b82bab8d8
stanleyBelo <stanleyhtmt@gmail.com>
2024-03-11 14:34:19 -0300

    .gitignore

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 function convertCurrency() {
-  const apiKey = '7a87a9679d0b2c962c754f6b'; 
+  const apiKey = document.getElementById('apiKey'); // enter your api key as a string
   const fromCurrency = document.getElementById('fromCurrency').value;
   const toCurrency = document.getElementById('toCurrency').value;
   const amount = document.getElementById('amount').value;
